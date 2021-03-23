7b66b5af25a1256c83ad732f0cd0d7e20cec0928
Kai <97007ken@gmail.com>
2021-03-23 15:40:24 +0800

    BoxTester

---
+++ src/main/java/BoxTester/Tester.java
@@ -0,0 +1,16 @@
+package BoxTester;
+
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        System.out.println("Please enter lenght : ");
+        float lenght = scanner.nextFloat();
+        System.out.println("Please enter width : ");
+        float width = scanner.nextFloat();
+        System.out.println("Please enter height : ");
+        float heright = scanner.nextFloat();
+    }
+}
