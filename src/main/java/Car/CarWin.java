7671929b679efafb494bfeddbf4cd4402e0e9105
Kai <97007ken@gmail.com>
2021-06-01 15:09:09 +0800

    Cars

---
+++ src/main/java/Car/CarWin.java
@@ -0,0 +1,16 @@
+package Car;
+
+import javax.swing.*;
+
+public class CarWin {
+
+    static int WIDTH = 600;
+    static int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
